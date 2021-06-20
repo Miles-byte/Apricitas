4f5990940baf908388a83c53fc6653fae8425804
Miles-byte <58167625+Miles-byte@users.noreply.github.com>
2021-06-20 10:04:40 -0400

    Create Keep the SuperDole - Forever.R

---
+++ Keep the SuperDole - Forever/Keep the SuperDole - Forever.R
@@ -0,0 +1,21 @@
+pacman::p_load(pacman,rio,ggplot2,ggthemes,quantmod,dplyr,data.table,lubridate,forecast,tidyr,zoo,RCurl)
+
+Yearly_NIPA_Aggregates <- read.csv("https://raw.githubusercontent.com/Miles-byte/Apricitas/main/Keep%20the%20SuperDole%20-%20Forever/Yearly%20Aggregates.csv")
+# create data
+time <- as.numeric(rep(seq(2015,2020),each=7))  # x axis = 7 to include personal income, pre existing transfers, traditional unemployment, expanded unemployment, checks, ppp, and other new programs
+value <- runif(49, 10, 100)               # y Axis
+group <- rep(LETTERS[1:7],times=7)        # group, one shape per group
+data <- data.frame(time, value, group)
+
+# stacked area chart
+ggplot(data, aes(x=time, y=value, fill=group)) + 
+  geom_area()
+
+# Clear packages
+p_unload(all)  # Remove all add-ons
+
+# Clear console
+cat("\014")  # ctrl+L
+
+rm(list = ls())
+
