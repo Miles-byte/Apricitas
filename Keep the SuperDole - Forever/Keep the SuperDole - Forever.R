7366486af4ceef450f1ffd92c1f4dc957d191d13
Miles-byte <58167625+Miles-byte@users.noreply.github.com>
2021-06-28 23:53:52 -0400

    SuperDole Data Update
    updating data

--- Keep the SuperDole - Forever/Keep the SuperDole - Forever.R
+++ Keep the SuperDole - Forever/Keep the SuperDole - Forever.R
@@ -2,11 +2,18 @@ pacman::p_load(pacman,rio,ggplot2,ggthemes,quantmod,dplyr,data.table,lubridate,f
 
 Yearly_NIPA_Aggregates <- read.csv("https://raw.githubusercontent.com/Miles-byte/Apricitas/main/Keep%20the%20SuperDole%20-%20Forever/Yearly%20Aggregates.csv")
 # create data
+Yearly_NIPA_Aggregates <- t(Yearly_NIPA_Aggregates)#transposing data
+
+colnames(Yearly_NIPA_Aggregates) <- Yearly_NIPA_Aggregates[1,] #moving text names to headers
+Yearly_NIPA_Aggregates <-Yearly_NIPA_Aggregates[-1,]
+
+?reshape()
 time <- as.numeric(rep(seq(2015,2020),each=7))  # x axis = 7 to include personal income, pre existing transfers, traditional unemployment, expanded unemployment, checks, ppp, and other new programs
 value <- runif(49, 10, 100)               # y Axis
 group <- rep(LETTERS[1:7],times=7)        # group, one shape per group
 data <- data.frame(time, value, group)
 
+?rep()
 # stacked area chart
 ggplot(data, aes(x=time, y=value, fill=group)) + 
   geom_area()
