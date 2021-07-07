09cede53578e226d78ad872f6aa839afce9b89be
Miles-byte <58167625+Miles-byte@users.noreply.github.com>
2021-07-07 16:58:23 -0400

    Initial Commit - Household Illiquidity

---
+++ Household Illiquidity/Household Illiquidity.R
@@ -0,0 +1,12 @@
+pacman::p_load(pacman,rio,ggplot2,ggthemes,quantmod,dplyr,scale,data.table,lubridate,forecast,gifski,av,tidyr,gganimate)
+
+
+
+
+# Clear packages
+p_unload(all)  # Remove all add-ons
+
+# Clear console
+cat("\014")  # ctrl+L
+
+rm(list = ls())
