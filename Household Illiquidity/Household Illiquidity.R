252dc3069f676b41dfa4cb97c4063c4dc62b5674
Miles-byte <58167625+Miles-byte@users.noreply.github.com>
2021-07-09 18:03:55 -0400

    Added a Graph of Illiquidity of Non-Retired Persons and Imported the Tax Foundation's graph of stimulus payments

--- Household Illiquidity/Household Illiquidity.R
+++ Household Illiquidity/Household Illiquidity.R
@@ -8,7 +8,6 @@ Emergency_Expenses20 <- crosstab(df = SHED_2020, x = ppincimp, y = EF1, weight =
 Emergency_Expenses_2020
 
 Emergency_Expenses19 <- crosstab(df = SHED_2019, x = ppincimp, y = EF1, weight = weight,format = "long") #taking crosstabs of EF1 "do you have an emergency fund of 3 months worth of expenses" and ppinc "household income"
-Emergency_Expenses_2019
 
 levels(Emergency_Expenses20$ppincimp) <- list("<5k" ="Less than $5,000", #renaming household income so it fits on the chart
                                                       "7k" = "$5,000 to $7,499",
@@ -60,7 +59,6 @@ levels(Emergency_Expenses19$ppincimp) <- list("<5k" ="Less than $5,000", #renami
 
 levels(Emergency_Expenses19$EF1) <- list( "Refused to Answer" = "Refused","No" = "No","Yes"="Yes" #renaming and reordering the answers
 )
-
 
 Emergency_Expenses_2020_Graph <- ggplot(Emergency_Expenses20, aes(x = ppincimp,pct/100, fill = EF1))+
   geom_bar(stat = "identity") +
@@ -134,6 +132,51 @@ Emergency_Expenses_19_Borrow_Graph <- ggplot(Emergency_Expenses_19_Borrow, aes(x
   labs(caption = "Graph created by @JosephPolitano using Federal Reserve data", subtitle = "Responses by Household Income, Only Those Who Did Not Have 3 Month Emergency Fund, Weighted, 2019") +
   theme_economist() +
   scale_fill_economist(name="")
+
+
+SHED2019Non_Retired <- SHED_2019[SHED_2019$ppwork != "Not working retired", ] #pulling only SHED data of those not retired so that I can graph the percent of them who could not cover 3 months' expenses
+
+SHED2019Non_Retired_Crosstabs <- crosstab(df = SHED2019Non_Retired, x = ppincimp, y = EF2, weight = weight,format = "long") #taking crosstabs of EF2 "could you cover a 3 month expense with borrowing/selling" and ppinc "household income"
+
+levels(SHED2019Non_Retired_Crosstabs$ppincimp) <- list("<5k" ="Less than $5,000", #renaming household income so it fits on the chart
+                                                      "7k" = "$5,000 to $7,499",
+                                                      "10k" = "$7,500 to $9,999",
+                                                      "12.5k" = "$10,000 to $12,499",
+                                                      "15k" = "$12,500 to $14,999",
+                                                      "20k" = "$15,000 to $19,999",
+                                                      "25k" = "$20,000 to $24,999",
+                                                      "30k" = "$25,000 to $29,999",
+                                                      "35k" = "$30,000 to $34,999",
+                                                      "40k" = "$35,000 to $39,999",
+                                                      "50k" = "$40,000 to $49,999",
+                                                      "60k" = "$50,000 to $59,999",
+                                                      "75k" = "$60,000 to $74,999",
+                                                      "85k" = "$75,000 to $84,999",
+                                                      "100k" = "$85,000 to $99,999",
+                                                      "125k" = "$100,000 to $124,999",
+                                                      "150k" = "$125,000 to $149,999",
+                                                      "175k" = "$150,000 to $174,999",
+                                                      "200k" = "$175,000 to $199,999",
+                                                      "250k" = "$200,000 to $249,999",
+                                                      ">250k" = "$250,000 or more")
+
+
+levels(SHED2019Non_Retired_Crosstabs$EF2) <- list( "Refused to Answer" = "Refused","No" = "No","Yes"="Yes" #renaming and reordering the answers
+)
+
+SHED2019Non_Retired_Crosstabs<- SHED2019Non_Retired_Crosstabs[SHED2019Non_Retired_Crosstabs$EF2 == "No", ] #filtering only those who could not afford 3 months expenses with emergency fund (from EF1) with those who could not cover thru selling/borrowing (EF2)
+
+Emergency_Expenses_19_Borrow_Not_Retired <- ggplot(SHED2019Non_Retired_Crosstabs, aes(x = ppincimp,pct/100, fill = EF2))+
+  geom_bar(stat = "identity") +
+  xlab("Household Income Bin") +
+  ylab("") +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1), limits = c(0,1)) +
+  ggtitle("Could Not Cover 3 Months' Expenses with Savings, Borrowing, or Selling") +
+  labs(caption = "Graph created by @JosephPolitano using Federal Reserve data", subtitle = "Of Those Not Retired, Responses by Household Income, Weighted, 2019") +
+  theme_economist() +
+  scale_fill_economist(name="")+
+  theme(legend.position = "none")
+
 
 Surprise_Expense <- select(SHED_2019,weight,ppincimp,EF3_a:EF3_h) #organizing the data for answers to the question "how would you pay for a $400 emergency expense" by income
 Surprise_Expense <- gather(Surprise_Expense, Answer, Unexpected_400, EF3_a:EF3_h) #gathering answers to $400 emergency question into one variable
@@ -215,6 +258,7 @@ ggsave(dpi = "retina",plot = Emergency_Expenses_19_Borrow_Graph, "2019 SHED Emer
 ggsave(dpi = "retina",plot = Emergency_Expenses_19_Borrow_Graph_No, "2019 SHED Emergency Funds Borrowing pctNo.png", type = "cairo-png") #CAIRO GETS RID OF THE ANTI ALIASING ISSUE
 ggsave(dpi = "retina",plot = Surprise_Expense_Crosstab_Graph, "$400 Emergency Expense Crosstab Graph.png",height = 5.76, width = 9.5, type = "cairo-png") #CAIRO GETS RID OF THE ANTI ALIASING ISSUE
 ggsave(dpi = "retina",plot = Delinquencies_Graph, "Delinquencies.png", type = "cairo-png") #CAIRO GETS RID OF THE ANTI ALIASING ISSUE
+ggsave(dpi = "retina",plot = Emergency_Expenses_19_Borrow_Not_Retired, "Emergency Expenses Borrow Not Retired.png", type = "cairo-png") #CAIRO GETS RID OF THE ANTI ALIASING ISSUE
 
 rlang::last_error()
 # Clear packages
