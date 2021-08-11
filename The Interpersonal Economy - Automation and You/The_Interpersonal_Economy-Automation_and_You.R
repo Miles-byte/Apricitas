e5b21d3d512928d2de7db55a4f44dd6d47c466f1
Miles-byte <58167625+Miles-byte@users.noreply.github.com>
2021-08-11 18:50:25 -0400

    Adding Labor and Capital Share of Income

--- The Interpersonal Economy - Automation and You/The_Interpersonal_Economy-Automation_and_You.R
+++ The Interpersonal Economy - Automation and You/The_Interpersonal_Economy-Automation_and_You.R
@@ -3,6 +3,14 @@ pacman::p_load(pacman,rio,ggplot2,ggthemes,quantmod,dplyr,data.table,lubridate,f
 
 Employ_by_sector <- read.csv("https://raw.githubusercontent.com/Miles-byte/Apricitas/main/The%20Interpersonal%20Economy%20-%20Automation%20and%20You/employment-by-economic-sector.csv") #importing data on jobs by sector from OWID
 
+GDP_Goods_Services <- read.csv("https://raw.githubusercontent.com/Miles-byte/Apricitas/main/The%20Interpersonal%20Economy%20-%20Automation%20and%20You/Services_Manufacturing_GDP.csv")
+
+GDP_Goods_Services$DATE <- as.Date(GDP_Goods_Services$DATE,"%m/%d/%Y")
+
+colnames(GDP_Goods_Services) <- c("DATE","Services Production","Goods Production","Services Consumption","Goods Consumption")
+GDP_Goods_Services <- pivot_longer(GDP_Goods_Services, cols = `Services Production`:`Goods Consumption`, names_to = "Sector", values_to = "Percent") #gathering data from sectoral employment to one column
+
+
 colnames(Employ_by_sector) <- c("Country","Country_Code","Year","Services","Manufacturing","Agriculture") #changing colnames to make more sense
 
 Employ_by_sector_percent <- Employ_by_sector #creaging a separate dataframe to calculate percentages
@@ -42,10 +50,36 @@ Employ_by_sector_percent_graph <- ggplot() +
   scale_fill_manual(name= NULL,values = c("#00A99D","#FFE98F","#EE6055","#A7ACD9","#9A348E")) + 
   scale_color_manual(name= NULL,values = c("#00A99D","#FFE98F","#EE6055","#A7ACD9","#9A348E"))
 
+ggplot() + 
+  geom_line(data = GDP_Goods_Services, aes(x=DATE, y = Services_Produced, color = "Services Production"),size = 1.25,stat = "density", alpha = 1) + 
+  geom_line(data = GDP_Goods_Services, aes(x=DATE, y = Manufacturing_Produced, color = "Goods Production"),size = 1.25, alpha = 1) + 
+  geom_line(data = GDP_Goods_Services, aes(x=DATE, y = Services_Consumed, color = "Services Consumption"),size = 1.25, alpha = 0.5) + 
+  geom_line(data = GDP_Goods_Services, aes(x=DATE, y = Manufacturing_Consumed, color = "Goods Consumption"),size = 1.25, alpha = 0.5) + 
+  xlab("Year") +
+  #scale_y_continuous(labels = scales::percent_format(),limits = c(0,1), breaks = c(0,0.25,0.5,.75,1), expand = c(0,0)) +
+  #scale_x_continuous(expand = c(0,0)) + #expand = 0,0 sets the margins for the chart between the axis and data points to 0
+  ylab("Employment by Sector, % of Total Employment") +
+  ggtitle("At Your Service") +
+  labs(caption = "Graph created by @JosephPolitano using OWID data based on Herrendorf et al. (2014)",subtitle = "The Service Sector Represents the Vast Majority of Employment in America") +
+  theme_apricitas + theme(legend.position = c(.85,.6)) +
+  #scale_color_manual(name= NULL,values = c("#FFE98F","#FFE98F","#EE6055","#EE6055"), guide=guide_legend(override.aes=list(alpha = (1,0.5,1,0.5))))
+  scale_color_manual(name= NULL,values = c("#FFE98F","#FFE98F","#EE6055","#EE6055"), guide=guide_legend(override.aes=list(alpha=c(1,.5,1,.5))))
+  
+GDP_Goods_Services_graph <- ggplot() + 
+  geom_line(data = GDP_Goods_Services, aes(x=DATE, y = Percent/100, color = Sector, alpha = Sector),size = 1.25) + 
+  xlab("Date") +
+  scale_y_continuous(labels = scales::percent_format(),limits = c(.15,.5), breaks = c(0.2,0.3,.4,.5), expand = c(0,0)) +
+  #scale_x_continuous(expand = c(0,0)) + #expand = 0,0 sets the margins for the chart between the axis and data points to 0
+  ylab("Consumption and Production by Sector, % of GDP") +
+  ggtitle("At Your Service") +
+  labs(caption = "Graph created by @JosephPolitano using BEA Data",subtitle = "The Service Sector Has Grown to Exceed Manufacturing Sector's Share of GDP") +
+  theme_apricitas + theme(legend.position = c(.85,.6)) +
+  scale_color_manual(name= NULL,values = c("#FFE98F","#FFE98F","#EE6055","#EE6055")) +#, guide=guide_legend(override.aes=list(alpha=c(1,.5,1,.5))))+
+  scale_alpha_manual(name= NULL,values = c(1,.5,1,.5))
+
 ggsave(dpi = "retina",plot = Employ_by_sector_graph, "Employ_by_sector.png", type = "cairo-png") #Saving Image of Employment by sector graph
 ggsave(dpi = "retina",plot = Employ_by_sector_percent_graph, "Employ_by_sector_percent.png", type = "cairo-png") #Saving Image of employment by sector % graph
-
-
+ggsave(dpi = "retina",plot = GDP_Goods_Services_graph, "GDP_Goods_Services.png", type = "cairo-png") #Saving Image of employment by sector % graph
 
 
 p_unload(all)  # Remove all add-ons
