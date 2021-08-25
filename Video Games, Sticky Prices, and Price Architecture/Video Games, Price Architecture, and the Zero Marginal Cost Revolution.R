4f1bee2633ad3c4cc6ac1e40968422b0054c6485
Miles-byte <58167625+Miles-byte@users.noreply.github.com>
2021-08-25 00:29:26 -0400

    Adding Video Games, Sticky Prices and Price Architecture

---
+++ Video Games, Sticky Prices, and Price Architecture/Video Games, Price Architecture, and the Zero Marginal Cost Revolution.R
@@ -0,0 +1,2 @@
+pacman::p_load(pacman,rio,ggplot2,ggthemes,quantmod,dplyr,data.table,lubridate,forecast,gifski,av,tidyr,gganimate,zoo,RCurl,Cairo,datetime,stringr,pollster,tidyquant,hrbrthemes,plotly)
+
