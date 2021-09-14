bd2d4a4ca1f3212e9cebaf3a7aed4cf4eb3e3008
Miles-byte <58167625+Miles-byte@users.noreply.github.com>
2021-09-14 00:03:14 -0400

    Adding Tealbook Estimates and Updating Code

--- The Fault in R-Stars/The Fault in R-Stars.R
+++ The Fault in R-Stars/The Fault in R-Stars.R
@@ -5,12 +5,15 @@ UNRATE <- fredr(series_id = c("UNRATE")) #downloading unemployment rate
 NROU_CBO <- fredr(series_id = "NROU") #downloading CBO's natural rate of unemployment estimates
 PCEPI <- fredr(series_id = "PCEPI",units = "pc1") #downloading PCEPI estimates
 Trimmed_PCEPI <- fredr(series_id = "PCETRIM12M159SFRBDAL") #downloading trimmed mean PCEPI estimates
+NAIRU_Vintage_CBO <- read.csv("https://raw.githubusercontent.com/Miles-byte/Apricitas/main/The%20Fault%20in%20R-Stars/CBO_Vintages.csv")
+
+NAIRU_Vintage_CBO$Date <- as.Date(NAIRU_Vintage_CBO$Date, "%m/%d/%Y")
 
 theme_apricitas <- theme_ft_rc() +
   theme(axis.line = element_line(colour = "white"),legend.position = c(.90,.90),legend.text = element_text(size = 11, color = "white")) #using the FT theme and white axis lines for a "theme_apricitas"
 
-apriticas_logo <- image_read("https://github.com/Miles-byte/Apricitas/blob/main/Logo.png?raw=true") #downloading and rasterizing Apricitas Logo from github
-apriticitas_logo_rast <- rasterGrob(apricitas_logo, interpolate=TRUE)
+apricitas_logo <- image_read("https://github.com/Miles-byte/Apricitas/blob/main/Logo.png?raw=true") #downloading and rasterizing Apricitas Logo from github
+apricitas_logo_rast <- rasterGrob(apricitas_logo, interpolate=TRUE)
 
 FOMC_UNRATE_GRAPH <- ggplot() + #plotting FOMC projections alongside Unemployment Rate
   geom_line(data=FOMC_Projections_UNRATE, aes(x=date,y=value/100,color= "Midpoint of FOMC Projections for Longer Run Unemployment Rate"), size = 1.25)+
@@ -23,7 +26,7 @@ FOMC_UNRATE_GRAPH <- ggplot() + #plotting FOMC projections alongside Unemploymen
   labs(caption = "Graph created by @JosephPolitano using BLS and Federal Reserve Data", subtitle = "As a Result, FOMC Projections of Long Term Unemployment Have Been Decreasing") +
   theme_apricitas + theme(legend.position = c(.50,.73)) +
   scale_color_manual(name= NULL,values = c("#FFE98F","#00A99D","#EE6055","#A7ACD9","#9A348E","#6A4C93"))+ 
-  annotation_custom(g, xmin = as.Date("2005-06-01"), xmax = as.Date("2007-05-01"), ymin = -0.005, ymax = .03) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("2005-06-01"), xmax = as.Date("2007-05-01"), ymin = -0.005, ymax = .03) +
   coord_cartesian(clip = "off")
 
 #Percentages for Logo Organization
@@ -42,16 +45,30 @@ CBO_NROU_GRAPH <- ggplot() + #plotting long run unemployment rate against CBO NR
   labs(caption = "Graph created by @JosephPolitano using BLS, CBO, BEA, and Federal Reserve Data", subtitle = "CBO's 'Natural' Unemployment Rate Estimate Does Not Predict Inflation") +
   theme_apricitas + theme(legend.position = c(.45,.85)) +
   scale_color_manual(name= NULL,breaks = c("Unemployment Rate", "CBO NAIRU Estimate", "PCE Inflation", "Trimmed Mean PCE Inflation"),values = c("#FFE98F","#00A99D","#EE6055","#A7ACD9","#9A348E","#6A4C93"))+ 
-  annotation_custom(g, xmin = as.Date("1990-01-01")-2038, xmax = as.Date("1990-01-01")-537, ymin = -0.02-0.036, ymax = -0.02) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("1990-01-01")-2038, xmax = as.Date("1990-01-01")-537, ymin = -0.02-0.036, ymax = -0.02) +
   coord_cartesian(clip = "off")
 
+CBO_Vintages_Graph <- ggplot() + #mention from potential GDP estimates
+  geom_line(data=NAIRU_Vintage_CBO, aes(x=Date,y=X2002_Vintage_NAIRU/100,color= "2002 Vintage NAIRU"), size = 1.25) +
+  geom_line(data=NAIRU_Vintage_CBO, aes(x=Date,y=X2007_Vintage_NAIRU/100,color= "2007 Vintage NAIRU"), size = 1.25) +
+  geom_line(data=NAIRU_Vintage_CBO, aes(x=Date,y=X2012_Vintage_NAIRU/100,color= "2012 Vintage NAIRU"), size = 1.25) +
+  geom_line(data=NAIRU_Vintage_CBO, aes(x=Date,y=X2017_Vintage_NAIRU/100,color= "2017 Vintage NAIRU"), size = 1.25) +
+  geom_line(data=NAIRU_Vintage_CBO, aes(x=Date,y=X2021_NAIRU/100,color= "2021 NAIRU"), size = 1.25) +
+  ylab("Unemployment/Inflation Rate, %") +
+  xlab("Date") +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 0.5),limits = c(.04,.06), expand = c(0,0)) +
+  scale_x_date(limits = c(as.Date("2000-01-01"),as.Date("2032-01-01"))) +
+  ggtitle("Un-Natural Fluctuations") +
+  labs(caption = "Graph created by @JosephPolitano using BLS, CBO, BEA, and Federal Reserve Data", subtitle = "CBO's NAIRU Estimates Rose After 2008 But Have Been Declining Since 2012") +
+  theme_apricitas + theme(legend.position = c(.85,.62)) +
+  scale_color_manual(name= NULL,values = c("#FFE98F","#00A99D","#EE6055","#A7ACD9","#9A348E","#6A4C93"))+ 
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("2000-01-01")-(.1861*11688), xmax = as.Date("2000-01-01")-(0.049*11688), ymin = 0.04-(.3*0.02), ymax = 0.04) +
+  coord_cartesian(clip = "off")
 
 
 ggsave(dpi = "retina",plot = FOMC_UNRATE_GRAPH, "FOMC.png", type = "cairo-png") 
 ggsave(dpi = "retina",plot = CBO_NROU_GRAPH, "CBO NROU Graph.png", type = "cairo-png") 
-
-
-warnings()
+ggsave(dpi = "retina",plot = CBO_Vintages_Graph, "CBO Vintages.png", type = "cairo-png") 
 
 p_unload(all)  # Remove all add-ons
 
