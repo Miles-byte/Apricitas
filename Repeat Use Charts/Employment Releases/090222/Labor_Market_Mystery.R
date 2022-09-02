84a596a0cfb90359db0c9b5790bf74e84e5e2ce1
Miles-byte <58167625+Miles-byte@users.noreply.github.com>
2022-09-02 19:35:44 -0400

    Adding ICE Inflation Expectations Data

---
+++ Repeat Use Charts/Employment Releases/090222/Labor_Market_Mystery.R
@@ -0,0 +1,181 @@
+pacman::p_load(seasonal,stringi,ggpubr,cli,remotes,magick,cowplot,knitr,ghostscript,png,httr,grid,usethis,pacman,rio,ggplot2,ggthemes,quantmod,dplyr,data.table,lubridate,forecast,gifski,av,tidyr,gganimate,zoo,RCurl,Cairo,datetime,stringr,pollster,tidyquant,hrbrthemes,plotly,fredr)
+
+theme_apricitas <- theme_ft_rc() + #setting the "apricitas" custom theme that I use for my blog
+  theme(axis.line = element_line(colour = "white"),legend.position = c(.90,.90),legend.text = element_text(size = 14, color = "white"), legend.title =element_text(size = 14),plot.title = element_text(size = 28, color = "white")) #using a modified FT theme and white axis lines for my "theme_apricitas"
+
+apricitas_logo <- image_read("https://github.com/Miles-byte/Apricitas/blob/main/Logo.png?raw=true") #downloading and rasterizing my "Apricitas" blog logo from github
+apricitas_logo_rast <- rasterGrob(apricitas_logo, interpolate=TRUE)
+
+install_github("keberwein/blscrapeR")
+library(blscrapeR)
+
+MULTIPLE_JOBHOLDERS <- fredr(series_id = "LNS12026619",observation_start = as.Date("2018-01-01")) #downloading Multiple Jobholders data
+SELF_EMPLOYED <- fredr(series_id = "LNS12027714",observation_start = as.Date("2018-01-01")) #downloading Self Employed data
+AGRICULTURAL <- fredr(series_id = "LNS12034560",observation_start = as.Date("2018-01-01")) #downloading Agricultural Workers
+
+PAYEMS <- fredr(series_id = "PAYEMS",observation_start = as.Date("2019-01-01"),realtime_start = NULL, realtime_end = NULL) #Nonfarm Payrolls
+ELEV <- fredr(series_id = "CE16OV",observation_start = as.Date("2019-01-01"),realtime_start = NULL, realtime_end = NULL) #Employment Levels
+CPSADJ <- bls_api("LNS16000000", startyear = 2019) %>% #headline cpiadj
+  mutate(date = as.Date(as.yearmon(paste(periodName, year), "%b %Y")))
+
+MULTIPLE_32022 <- fredr(series_id = "LNS12026619",observation_start = as.Date("2022-04-01"), units = "chg")%>%
+  select(value,date) %>%
+  mutate(CUMSUM = cumsum(value))%>%
+  mutate(source = "Multiple Jobholders")
+SELF_32022 <- fredr(series_id = "LNS12027714",observation_start = as.Date("2022-04-01"), units = "chg")%>%
+  select(value,date) %>%
+  mutate(CUMSUM = cumsum(value))%>%
+  mutate(source = "Unincorporated Self Employed")
+AGRICULTURAL_32022 <- fredr(series_id = "LNS12034560",observation_start = as.Date("2022-04-01"), units = "chg")%>%
+  select(value,date) %>%
+  mutate(CUMSUM = cumsum(value))%>%
+  mutate(source = "Employment Level, Agricultural and Related Industries")
+
+
+PAYEMS_32022 <- fredr(series_id = "PAYEMS",observation_start = as.Date("2022-04-01"), units = "chg")%>%
+  select(value,date) %>%
+  mutate(CUMSUM = cumsum(value))%>%
+  mutate(source = "Nonfarm Payrolls")
+ELEV_32022 <- fredr(series_id = "CE16OV",observation_start = as.Date("2022-04-01"), units = "chg") %>%
+  select(value,date) %>%
+  mutate(CUMSUM = cumsum(value))%>%
+  mutate(source = "Employment Level")
+
+#
+CPSADJ_32022 <- subset(CPSADJ, date > as.Date("2022-02-01")) %>%
+  mutate(value = rev(value)) %>%
+  mutate(date = rev(date)) %>%
+  select(value,date) %>%
+  mutate(value = c(value-lag(.$value))) %>%
+  subset(date > as.Date("2022-03-01")) %>%
+  mutate(CUMSUM = cumsum(value)) %>%
+  mutate(source = "Household Survey Adjusted to Nonfarm Payrolls Concepts")
+
+#Merging nonfarm payrolls, cps adjusted to nonfarm payrolls, and employment levels growth since March 2022
+PAYEMS_ELEV_CPSADJ_32022 <- rbind(PAYEMS_32022,ELEV_32022,CPSADJ_32022) %>%
+  subset(date > as.Date(today()-60))
+
+DISCREPANCY_MEGA_MERGE <- rbind(PAYEMS_ELEV_CPSADJ_32022,MULTIPLE_32022,SELF_32022,AGRICULTURAL_32022) %>%
+  subset(date > as.Date(today()-60)) %>%
+  select(date,CUMSUM,source) %>%
+  pivot_wider(names_from=c(source), values_from = CUMSUM) %>%
+  mutate(`Agriculture and Related Industries` = -`Employment Level, Agricultural and Related Industries`) %>%
+  mutate(`Remaining Discrepancy`=`Nonfarm Payrolls`-`Household Survey Adjusted to Nonfarm Payrolls Concepts`) %>%
+  mutate(`Other Concept Differences` = + `Household Survey Adjusted to Nonfarm Payrolls Concepts` - (`Employment Level` + `Multiple Jobholders`- `Unincorporated Self Employed` - `Employment Level, Agricultural and Related Industries`)) %>%
+  mutate(`Unincorporated Self Employed` = -`Unincorporated Self Employed`)%>%
+  select(-`Household Survey Adjusted to Nonfarm Payrolls Concepts`,-`Nonfarm Payrolls`,-`Employment Level`,-`Employment Level, Agricultural and Related Industries`) %>%
+  pivot_longer(cols = c(`Multiple Jobholders`:`Other Concept Differences`)) %>%
+  mutate(factor = as.numeric(c("1","1","1","1","1"))) %>%
+  mutate(name = factor(name,levels = c("Remaining Discrepancy","Other Concept Differences","Unincorporated Self Employed","Multiple Jobholders","Agriculture and Related Industries")))
+  
+PAYEMS_ELEV_CPSADJ_32022_Graph <- ggplot(data = PAYEMS_ELEV_CPSADJ_32022, aes(x = date, y = CUMSUM/1000, fill = source)) + #plotting permanent and temporary job losers
+  annotate("hline", y = 0, yintercept = 0, color = "white", size = .5) +
+  geom_bar(stat = "identity", position = position_dodge(), color = NA) +
+  xlab("Date") +
+  ylab("Growth Since March 2022") +
+  scale_y_continuous(labels = scales::number_format(suffix = "M", accuracy = 0.5), breaks = c(0,.5,1,1.5,2), limits = c(-0.2,2.1), expand = c(0,0)) +
+  ggtitle("Growth Since March 2022") +
+  labs(caption = "Graph created by @JosephPolitano using BLS data", subtitle = "Different Sources Give Wildly Different Stories of the Labor Market Since March") +
+  theme_apricitas + theme(legend.position = c(.40,.90),axis.text.x=element_blank(), axis.title.x=element_blank()) +
+  scale_fill_manual(name= NULL,values = c("#FFE98F","#00A99D","#EE6055","#A7ACD9")) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("2022-07-01")-(.1861*(today()-as.Date("2022-07-01"))), xmax = as.Date("2022-07-01")-(0.049*(today()-as.Date("2022-07-01"))), ymin = -0.2-(.3*2.3), ymax = -0.2) +
+  coord_cartesian(clip = "off")
+
+TOTAL_DISCREPANCY_32022_Graph <- ggplot(data = DISCREPANCY_MEGA_MERGE, aes(x = factor, y = value/1000, fill = name)) + #plotting permanent and temporary job losers
+  annotate("hline", y = 0, yintercept = 0, color = "white", size = .5) +
+  geom_col(stat = "identity", position = "stack", color = NA, width = 0.5) +
+  xlab("Date") +
+  ylab("Contribution to Gap") +
+  scale_y_continuous(labels = scales::number_format(suffix = "M", accuracy = 0.5), breaks = c(0,.5,1,1.5,2), limits = c(-0.2,2.1), expand = c(0,0)) +
+  scale_x_continuous(limits = c(-0.5,2)) +
+  ggtitle("Breaking Down the Gap") +
+  labs(caption = "Graph created by @JosephPolitano using BLS data", subtitle = "The Emerging Gap Between Employment and Payrolls is Primarily About Results, not Concepts") +
+  theme_apricitas + theme(legend.position = c(.25,.60), axis.text.x=element_blank(), axis.title.x=element_blank()) +
+  scale_fill_manual(name= NULL,values = c("#FFE98F","#9A348E","#EE6055","#00A99D","#A7ACD9","#3083DC")) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("2022-07-01")-(.1861*(today()-as.Date("2022-07-01"))), xmax = as.Date("2022-07-01")-(0.049*(today()-as.Date("2022-07-01"))), ymin = -0.2-(.3*2.3), ymax = -0.2) +
+  coord_cartesian(clip = "off")
+
+
+MULTIPLE_JOBHOLDERS_Graph <- ggplot() + #plotting permanent and temporary job losers
+  geom_line(data=MULTIPLE_JOBHOLDERS, aes(x=date,y= value/1000,color= "Multiple Jobholders"), size = 1.25)+ 
+  xlab("Date") +
+  ylab("Millions of People") +
+  scale_y_continuous(labels = scales::number_format(suffix = "M", accuracy = 1), breaks = c(5,6,7,8,9,10), limits = c(5,9), expand = c(0,0)) +
+  ggtitle("Doubling Up") +
+  labs(caption = "Graph created by @JosephPolitano using BLS data", subtitle = "Multiple-Jobholding is Up After Dropping Precipitously at the Start of COVID") +
+  theme_apricitas + theme(legend.position = c(.70,.87)) +
+  scale_color_manual(name= NULL,values = c("#FFE98F","#00A99D","#EE6055","#A7ACD9")) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("2018-01-01")-(.1861*(today()-as.Date("2018-01-01"))), xmax = as.Date("2018-01-01")-(0.049*(today()-as.Date("2018-01-01"))), ymin = 5-(.3*4), ymax = 5) +
+  coord_cartesian(clip = "off")
+
+SELF_EMPLOYED_Graph <- ggplot() + #plotting permanent and temporary job losers
+  geom_line(data=SELF_EMPLOYED, aes(x=date,y= value/1000,color= "Unincorporated Self Employed"), size = 1.25)+ 
+  xlab("Date") +
+  ylab("Millions of People") +
+  scale_y_continuous(labels = scales::number_format(suffix = "M", accuracy = 1), breaks = c(8,9,10), limits = c(8,10.5), expand = c(0,0)) +
+  ggtitle("Freelance Flops") +
+  labs(caption = "Graph created by @JosephPolitano using BLS data", subtitle = "Self-Employment is Falling After Spiking at the Beginning of COVID") +
+  theme_apricitas + theme(legend.position = c(.30,.89)) +
+  scale_color_manual(name= NULL,values = c("#FFE98F","#00A99D","#EE6055","#A7ACD9")) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("2018-01-01")-(.1861*(today()-as.Date("2018-01-01"))), xmax = as.Date("2018-01-01")-(0.049*(today()-as.Date("2018-01-01"))), ymin = 8-(.3*2.5), ymax = 8) +
+  coord_cartesian(clip = "off")
+
+AGRICULTURAL_Graph <- ggplot() + #plotting permanent and temporary job losers
+  geom_line(data=AGRICULTURAL, aes(x=date,y= value/1000,color= "Employment Level, Agricultural and Related Industries"), size = 1.25)+ 
+  xlab("Date") +
+  ylab("Millions of People") +
+  scale_y_continuous(labels = scales::number_format(suffix = "M", accuracy = 0.1), breaks = c(2,2.2,2.4,2.6,2.8,3), limits = c(2,2.6), expand = c(0,0)) +
+  ggtitle("Tending the Farm") +
+  labs(caption = "Graph created by @JosephPolitano using BLS data", subtitle = "Agricultural Employment is Holding Approximately Steady") +
+  theme_apricitas + theme(legend.position = c(.65,.94)) +
+  scale_color_manual(name= NULL,values = c("#FFE98F","#00A99D","#EE6055","#A7ACD9")) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("2018-01-01")-(.1861*(today()-as.Date("2018-01-01"))), xmax = as.Date("2018-01-01")-(0.049*(today()-as.Date("2018-01-01"))), ymin = 2-(.3*0.6), ymax = 2) +
+  coord_cartesian(clip = "off")
+
+EMPLOYMENT_Graph <- ggplot() + #CPS with NFP adjusted concepts
+  geom_line(data = PAYEMS, aes(x=date, y = value/1000, color = "Nonfarm Payrolls"), size = 1.25) + 
+  geom_line(data = CPSADJ, aes(x=date, y = value/1000, color = "Household Survey Adjusted to Nonfarm Payrolls Concepts"), size = 1.25) + 
+  annotate(geom = "vline", x = as.Date("2022-03-01"), xintercept = as.Date("2022-03-01"), color = "white", linetype = "dashed", size = 1.25) +
+  annotate(geom = "text", label = "March 2022", x = as.Date("2021-12-01"), y = 135, color ="white", size = 4) +
+  xlab("Date") +
+  scale_y_continuous(labels = scales::number_format(suffix = "M"),limits = c(120,160), breaks = c(120,130,140,150,160), expand = c(0,0)) +
+  ylab("Payrolls/Employees, Millions") +
+  ggtitle("Labor Market Mystery Hour") +
+  labs(caption = "Graph created by @JosephPolitano using BLS data",subtitle = "The Gap Persists Even When Household Survey Data is Adjusted to Payrolls Concepts") +
+  theme_apricitas + theme(legend.position = c(.45,.92)) +
+  scale_color_manual(name= NULL,values = c("#FFE98F","#00A99D","#00A99D")) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("2019-01-01")-(.1861*(today()-as.Date("2019-01-01"))), xmax = as.Date("2019-01-01")-(0.049*(today()-as.Date("2019-01-01"))), ymin = 120-(.3*40), ymax = 120) + #these repeated sections place the logo in the bottom-right of each graph. The first number in all equations is the chart's origin point, and the second number is the exact length of the x or y axis
+  coord_cartesian(clip = "off")
+
+EMPLOYMENT_INDEX_Graph <- ggplot() + #indexed employment rate
+  geom_line(data = PAYEMS, aes(x=date, y = value/1521.28, color = "Nonfarm Payrolls (Establishment Survey)"), size = 1.25) + 
+  geom_line(data = ELEV, aes(x=date, y = value/1586.53, color = "Employment Level (Household Survey)"), size = 1.25) + 
+  annotate(geom = "vline", x = as.Date("2022-03-01"), xintercept = as.Date("2022-03-01"), color = "white", linetype = "dashed", size = 1.25) +
+  annotate(geom = "text", label = "March 2022", x = as.Date("2021-12-01"), y = 90, color ="white", size = 4) +
+  xlab("Date") +
+  scale_y_continuous(limits = c(82,105), breaks = c(85,90,95,100,105), expand = c(0,0)) +
+  ylab("Index, Jan 2020 = 100") +
+  ggtitle("Labor Market Mystery Hour") +
+  labs(caption = "Graph created by @JosephPolitano using BLS data",subtitle = "The Establishment Survey Shows Positive Growth, but the Household Survey Shows a Stall") +
+  theme_apricitas + theme(legend.position = c(.50,.90)) +
+  scale_color_manual(name= NULL,values = c("#FFE98F","#00A99D","#00A99D")) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("2019-01-01")-(.1861*(today()-as.Date("2019-01-01"))), xmax = as.Date("2019-01-01")-(0.049*(today()-as.Date("2019-01-01"))), ymin = 82-(.3*23), ymax = 82) + #these repeated sections place the logo in the bottom-right of each graph. The first number in all equations is the chart's origin point, and the second number is the exact length of the x or y axis
+  coord_cartesian(clip = "off")
+
+ggsave(dpi = "retina",plot = MULTIPLE_JOBHOLDERS_Graph, "Multiple Jobholders.png", type = "cairo-png") #CAIRO GETS RID OF THE ANTI ALIASING ISSUE
+ggsave(dpi = "retina",plot = SELF_EMPLOYED_Graph, "Self Employed.png", type = "cairo-png") #CAIRO GETS RID OF THE ANTI ALIASING ISSUE
+ggsave(dpi = "retina",plot = AGRICULTURAL_Graph, "Agricultural.png", type = "cairo-png") #CAIRO GETS RID OF THE ANTI ALIASING ISSUE
+ggsave(dpi = "retina",plot = EMPLOYMENT_Graph, "Employment.png", type = "cairo-png") #CAIRO GETS RID OF THE ANTI ALIASING ISSUE
+ggsave(dpi = "retina",plot = EMPLOYMENT_INDEX_Graph, "Employment Indexed.png", type = "cairo-png") #CAIRO GETS RID OF THE ANTI ALIASING ISSUE
+ggsave(dpi = "retina",plot = PAYEMS_ELEV_CPSADJ_32022_Graph, "Payems Elev CPSADJ.png", type = "cairo-png") #CAIRO GETS RID OF THE ANTI ALIASING ISSUE
+ggsave(dpi = "retina",plot = TOTAL_DISCREPANCY_32022_Graph, "Total Discrepancy.png", type = "cairo-png") #CAIRO GETS RID OF THE ANTI ALIASING ISSUE
+
+
+p_unload(all)  # Remove all add-ons
+
+# Clear console
+cat("\014")  # ctrl+L
+
+rm(list = ls())
+
+dev.off()
--- Understanding Inflation Expectations/Understanding Inflation Expectations.R
+++ Understanding Inflation Expectations/Understanding Inflation Expectations.R
@@ -6,6 +6,124 @@ theme_apricitas <- theme_ft_rc() +
 apricitas_logo <- image_read("https://github.com/Miles-byte/Apricitas/blob/main/Logo.png?raw=true") #downloading and rasterizing my "Apricitas" blog logo from github
 apricitas_logo_rast <- rasterGrob(apricitas_logo, interpolate=TRUE)
 
+#umich durable buying conditions and buy in advance of rising prices
+UMICH <- read.csv("https://raw.githubusercontent.com/Miles-byte/Apricitas/main/Understanding%20Inflation%20Expectations/UMICH.csv") %>%
+  mutate(date = as.Date(ï..date)) %>%
+  mutate(AVG_BIAP = (HOM_BIAP + CAR_BIAP + DUR_BIAP)/3)
+#umich inflation expectations
+UMICH_IE <- read.csv("https://raw.githubusercontent.com/Miles-byte/Apricitas/4555697f56553d9e73eb11b5262952f3e28b7361/Understanding%20Inflation%20Expectations/UMICH_IE.csv") %>%
+  mutate(date = as.Date(ï..date))
+
+#Adding Atlanta Fed Business Inflation Expectations
+BIE_1YR <- read.csv("https://raw.githubusercontent.com/Miles-byte/Apricitas/main/Understanding%20Inflation%20Expectations/ATL_BIE_1YR.csv") %>%
+  mutate(date = as.Date(ï..date))
+BIE_5YR <- read.csv("https://raw.githubusercontent.com/Miles-byte/Apricitas/main/Understanding%20Inflation%20Expectations/ATL_BIE_5YR.csv") %>%
+  mutate(date = as.Date(ï..date))
+#adding Gorodnichenko firm inflation expectations
+SOFIE_1YR <- read.csv("https://raw.githubusercontent.com/Miles-byte/Apricitas/main/Understanding%20Inflation%20Expectations/SOFIE_1YR.csv") %>%
+  mutate(date = as.Date(ï..date))
+SOFIE_5YR <- read.csv("https://raw.githubusercontent.com/Miles-byte/Apricitas/main/Understanding%20Inflation%20Expectations/SOFIE_5YR.csv") %>%
+  mutate(date = as.Date(ï..date))
+
+FIVEYEARBREAKEVEN2019 <- fredr(series_id = "T5YIE", observation_start = as.Date("2019-01-01"),realtime_start = NULL, realtime_end = NULL) #5 Year Inflation Breakevens data
+FIVEYEARFWDBREAKEVEN2019 <- fredr(series_id = "T5YIFR",observation_start = as.Date("2019-01-01"),realtime_start = NULL, realtime_end = NULL) # 5 year 5 year forward breakevens data
+FIVEYEARBREAKEVEN2019 <- drop_na(FIVEYEARBREAKEVEN2019)
+FIVEYEARFWDBREAKEVEN2019 <- drop_na(FIVEYEARFWDBREAKEVEN2019)
+
+
+UMICH_BUYING_Graph <- ggplot() + #plotting total quits
+  annotate("hline", y = 0, yintercept = 100, color = "white", size = .5) +
+  geom_line(data=drop_na(select(UMICH,date,DUR_COND)), aes(x=date,y= DUR_COND,color= "Household Durables"), size = 1.25)+ 
+  geom_line(data=drop_na(select(UMICH,date,CAR_CON)), aes(x=date,y= CAR_CON,color= "Motor Vehicles"), size = 1.25)+ 
+  geom_line(data=drop_na(select(UMICH,date,HOM_CON)), aes(x=date,y= HOM_CON,color= "Homes"), size = 1.25)+ 
+  xlab("Date") +
+  ylab("Index, 100 = Neutral") +
+  scale_y_continuous(labels = scales::number_format(accuracy = 1), breaks = c(40,60,80,100,120,140,160,180), limits = c(40,180), expand = c(0,0)) +
+  ggtitle("Buyer Beware") +
+  labs(caption = "Graph created by @JosephPolitano using UMich data", subtitle = "Consumers Rate Buying Conditions for All Major Purchases Near All-Time Lows") +
+  theme_apricitas + theme(legend.position = c(.63,.25)) +
+  scale_color_manual(name= "Buying Conditions: 3M Moving Average",values = c("#FFE98F","#00A99D","#EE6055","#A7ACD9")) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("1960-01-01")-(.1861*(today()-as.Date("1960-01-01"))), xmax = as.Date("1960-01-01")-(0.049*(today()-as.Date("1960-01-01"))), ymin = 40-(.3*140), ymax = 40) +
+  coord_cartesian(clip = "off")
+
+UMICH_BIAP_Graph <- ggplot() + #plotting total quits
+  geom_line(data=drop_na(select(UMICH,date,DUR_BIAP)), aes(x=date,y= DUR_BIAP/100,color= "Household Durables"), size = 1.25)+ 
+  geom_line(data=drop_na(select(UMICH,date,CAR_BIAP)), aes(x=date,y= CAR_BIAP/100,color= "Motor Vehicles"), size = 1.25)+ 
+  geom_line(data=drop_na(select(UMICH,date,HOM_BIAP)), aes(x=date,y= HOM_BIAP/100,color= "Homes"), size = 1.25)+ 
+  xlab("Date") +
+  ylab("Percent") +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1), breaks = c(0,.10,.20,.30,.40,.50), limits = c(0,.50), expand = c(0,0)) +
+  ggtitle("What Were You Expecting?") +
+  labs(caption = "Graph created by @JosephPolitano using UMich data", subtitle = "Consumers Aren't Rushing Out to Make Big Purchases Before Prices Rise") +
+  theme_apricitas + theme(legend.position = c(.75,.84)) +
+  scale_color_manual(name= "Good Time to Buy: Prices are Going Up",values = c("#FFE98F","#00A99D","#EE6055","#A7ACD9")) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("1960-01-01")-(.1861*(today()-as.Date("1960-01-01"))), xmax = as.Date("1960-01-01")-(0.049*(today()-as.Date("1960-01-01"))), ymin = 0-(.3*.50), ymax = 0) +
+  coord_cartesian(clip = "off")
+
+UMICH_BIAP_IE_Graph <- ggplot() + #plotting total quits
+  geom_line(data=drop_na(select(UMICH,date,AVG_BIAP)), aes(x=date,y= AVG_BIAP/100*.25,color= "Good Time to Buy: Prices are Going Up, Major Purchases (rhs)"), size = 1.25)+ 
+  geom_line(data=drop_na(select(UMICH_IE,date,PX_MD)), aes(x=date,y= PX_MD/100,color= "Inflation Expectations: Next Year (lhs)"), size = 1.25)+ 
+  geom_line(data=drop_na(select(UMICH_IE,date,PX5_MD)), aes(x=date,y= PX5_MD/100,color= "Inflation Expectations: Next 5-10 Years (lhs)"), size = 1.25)+ 
+  xlab("Date") +
+  ylab("Percent") +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1), limits = c(0,.10), breaks = c(0,.05,.1,.15), expand = c(0,0), sec.axis = sec_axis(~.*1/.25, name="Percent",labels = scales::percent_format(accuracy = 1))) +
+  scale_x_date(limits = c(as.Date("1975-01-01"),today()),breaks = as.Date(c("1970-01-01","1980-01-01","1990-01-01","2000-01-01","2010-01-01","2020-01-01")), date_labels = "%Y") +
+  ggtitle("What Were You Expecting?") +
+  labs(caption = "Graph created by @JosephPolitano using UMich data", subtitle = "Despite Rising Inflation Expectations, Consumers Aren't Rushing to Make Big Purchases") +
+  theme_apricitas + theme(legend.position = c(.57,.84), legend.text = element_text(size = 13, color = "white")) +
+  scale_color_manual(name= NULL,values = c("#FFE98F","#00A99D","#EE6055","#A7ACD9"), breaks = c("Good Time to Buy: Prices are Going Up, Major Purchases (rhs)","Inflation Expectations: Next Year (lhs)","Inflation Expectations: Next 5-10 Years (lhs)")) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("1975-01-01")-(.1861*(today()-as.Date("1975-01-01"))), xmax = as.Date("1975-01-01")-(0.049*(today()-as.Date("1975-01-01"))), ymin = 0-(.3*.10), ymax = 0) +
+  coord_cartesian(clip = "off")
+
+BIE_Graph <- ggplot() + #plotting total quits
+  geom_line(data=BIE_1YR, aes(x=date,y= bie,color= "Business Unit Cost Inflation Expectations: Next Year"), size = 1.25)+ 
+  geom_line(data=BIE_5YR, aes(x=date,y= bie_5yr,color= "Business Unit Cost Inflation Expectations: Next 5-10 Years"), size = 1.25)+ 
+  xlab("Date") +
+  ylab("Percent") +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1), limits = c(0,.05), breaks = c(0,.01,.02,.03,.04,.05), expand = c(0,0)) +
+  ggtitle("What Were You Expecting?") +
+  labs(caption = "Graph created by @JosephPolitano using Atlanta Fed data", subtitle = "Business Unit Cost Inflation Expectations Are Maybe More Accurate Predictions") +
+  theme_apricitas + theme(legend.position = c(.47,.84)) +
+  scale_color_manual(name= NULL,values = c("#FFE98F","#00A99D","#EE6055","#A7ACD9"), breaks = c("Business Unit Cost Inflation Expectations: Next Year","Business Unit Cost Inflation Expectations: Next 5-10 Years")) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("2011-10-21")-(.1861*(today()-as.Date("2011-10-21"))), xmax = as.Date("2011-10-21")-(0.049*(today()-as.Date("2011-10-21"))), ymin = 0-(.3*.05), ymax = 0) +
+  coord_cartesian(clip = "off")
+
+SOFIE_Graph <- ggplot() + #plotting total quits
+  geom_line(data=SOFIE_1YR, aes(x=date,y= mean/100,color= "Firm Inflation Expectations: Next Year"), size = 1.25)+ 
+  geom_line(data=SOFIE_5YR, aes(x=date,y= X5yr/100,color= "Firm Inflation Expectations: Next 5 Years"), size = 1.25)+ 
+  xlab("Date") +
+  ylab("Percent") +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1), limits = c(0,.08), breaks = c(0,.01,.02,.03,.04,.05,0.06,0.07,0.08), expand = c(0,0)) +
+  ggtitle("What Were You Expecting?") +
+  labs(caption = "Graph created by @JosephPolitano using SoFIE data", subtitle = "Even CEOs Don't Have Anchored Inflation Expectations") +
+  theme_apricitas + theme(legend.position = c(.47,.84)) +
+  scale_color_manual(name= NULL,values = c("#FFE98F","#00A99D","#EE6055","#A7ACD9"), breaks = c("Firm Inflation Expectations: Next Year","Firm Inflation Expectations: Next 5 Years")) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("2018-04-01")-(.1861*(today()-as.Date("2018-04-01"))), xmax = as.Date("2018-04-01")-(0.049*(today()-as.Date("2018-04-01"))), ymin = 0-(.3*.08), ymax = 0) +
+  coord_cartesian(clip = "off")
+
+T5YIE2019 <- ggplot() + #plotting inflation breakevens
+  annotate("rect", xmin = as.Date(-Inf), xmax = as.Date(Inf), ymin = 0.0225, ymax = 0.0275, fill = "#EE6055", color = NA, alpha = 0.4) +
+  geom_line(data=FIVEYEARBREAKEVEN2019, aes(x=date,y= value/100 ,color= "5 Year Inflation Breakevens"), size = 1.25) +
+  geom_line(data=FIVEYEARFWDBREAKEVEN2019, aes(x=date,y= value/100 ,color= "5 Year, 5 Year Forward Inflation Breakevens"), size = 1.25) +
+  annotate("text", label = "Breakevens Approximately Consistent With 2% Inflation Target", x = as.Date("2020-01-01"), y = 0.0287, color = "#EE6055", alpha = 0.6, size = 4) +
+  xlab("Date") +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1), limits = c(0,.038), breaks = c(0,0.01,0.02,0.03), expand = c(0,0)) +
+  ylab("TIPS Breakevens, %") +
+  ggtitle("Here's A Tip:") +
+  labs(caption = "Graph created by @JosephPolitano using Federal Reserve data",subtitle = "Short and Long Term Inflation Expectations are Stabilizing") +
+  theme_apricitas + theme(legend.position = c(.40,.90)) +
+  scale_color_manual(name= NULL,values = c("#FFE98F","#00A99D","#FFE98F","#EE6055","#A7ACD9","#9A348E")) +
+  theme(legend.key.width =  unit(.82, "cm")) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("2019-01-01")-(.1861*(today()-as.Date("2019-01-01"))), xmax = as.Date("2019-01-01")-(0.049*(today()-as.Date("2019-01-01"))), ymin = 0-(.3*.038), ymax = 0) +
+  coord_cartesian(clip = "off")
+
+ggsave(dpi = "retina",plot = UMICH_BUYING_Graph, "UMICH BUYING.png", type = "cairo-png") #CAIRO GETS RID OF THE ANTI ALIASING ISSUE
+ggsave(dpi = "retina",plot = UMICH_BIAP_Graph, "UMICH BIAP GRAPH.png", type = "cairo-png") #CAIRO GETS RID OF THE ANTI ALIASING ISSUE
+ggsave(dpi = "retina",plot = UMICH_BIAP_IE_Graph, "UMICH BIAP IE.png", type = "cairo-png") #CAIRO GETS RID OF THE ANTI ALIASING ISSUE
+ggsave(dpi = "retina",plot = BIE_Graph, "BIE Graph.png", type = "cairo-png") #CAIRO GETS RID OF THE ANTI ALIASING ISSUE
+ggsave(dpi = "retina",plot = SOFIE_Graph, "SoFIE Graph.png", type = "cairo-png") #CAIRO GETS RID OF THE ANTI ALIASING ISSUE
+ggsave(dpi = "retina",plot = T5YIE2019, "T5YIE2019.png", type = "cairo-png") #CAIRO GETS RID OF THE ANTI ALIASING ISSUE
+
 
 p_unload(all)  # Remove all add-ons
 
