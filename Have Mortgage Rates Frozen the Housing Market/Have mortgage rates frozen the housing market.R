790f8f9fea4f5027685166692aaa3d5490767bd6
Miles-byte <58167625+Miles-byte@users.noreply.github.com>
2023-09-01 07:53:40 -0400

    Updating Housing Data

--- Have Mortgage Rates Frozen the Housing Market/Have mortgage rates frozen the housing market.R
+++ Have Mortgage Rates Frozen the Housing Market/Have mortgage rates frozen the housing market.R
@@ -1,4 +1,4 @@
-pacman::p_load(openxlsx,sf,tidycensus,cli,remotes,magick,cowplot,knitr,ghostscript,png,httr,grid,usethis,pacman,rio,ggplot2,ggthemes,quantmod,dplyr,data.table,lubridate,forecast,gifski,av,tidyr,gganimate,zoo,RCurl,Cairo,datetime,stringr,pollster,tidyquant,hrbrthemes,plotly,fredr)
+pacman::p_load(zctaCrosswalk,zipcodeR,nngeo,ggpubr,sf,tigris,maps,mapproj,usmap,fips,openxlsx,sf,tidycensus,cli,remotes,magick,cowplot,knitr,ghostscript,png,httr,grid,usethis,pacman,rio,ggplot2,ggthemes,quantmod,dplyr,data.table,lubridate,forecast,gifski,av,tidyr,gganimate,zoo,RCurl,Cairo,datetime,stringr,pollster,tidyquant,hrbrthemes,plotly,fredr)
 
 theme_apricitas <- theme_ft_rc() +
   theme(axis.line = element_line(colour = "white"),legend.position = c(.90,.90),legend.text = element_text(size = 14, color = "white"), legend.title =element_text(size = 14),plot.title = element_text(size = 28, color = "white")) #using a modified FT theme and white axis lines for my "theme_apricitas"
@@ -43,7 +43,7 @@ HOMEOWN_FREE_CLEAR_PCT_MAP <- ggplot() +
                     labels = c("<30%", "30-35%", "35-40%", "40-45%", ">45%")) +
   ggtitle("    Percent of Homeowners Without a Mortgage") +
   theme(plot.title = element_text(size = 24)) +
-  labs(caption = "Graph created by @JosephPolitano using Census ACS data") +
+  labs(caption = "Graph created by @JosephPolitano using Census ACS 1-Year data") +
   labs(fill = NULL) +
   theme_apricitas + theme(legend.position = "right", panel.grid.major=element_blank(), axis.line = element_blank(), axis.text.x = element_blank(),axis.text.y = element_blank(),plot.margin= grid::unit(c(0, 0, 0, 0), "in"))
 
@@ -189,7 +189,7 @@ COUNTY_FREE_CLEAR_REG_graph <- ggplot() + #plotting traditional Unemployment/PCE
   scale_x_continuous(labels = scales::percent_format(accuracy = 1),limits = c(0.125,.9), expand = c(0,0)) +
   scale_y_continuous(labels = scales::percent_format(accuracy = 1),limits = c(-.75,.750), expand = c(0,0), breaks = c(-.75,-.5,-.25,0,0.25,0.5,0.75)) +
   ggtitle("Change in For-Sale Inventory vs\nShare of Homeowners Without Mortgages") +
-  labs(caption = "Graph created by @JosephPolitano using Redfin and Census data", subtitle = "The Relationship Between Inventory Growth and Free-and Clear Ownership is Still Weak") +
+  labs(caption = "Graph created by @JosephPolitano using Redfin and Census ACS 5-Year data", subtitle = "The Relationship Between Inventory Growth and Free-and Clear Ownership is Still Weak") +
   theme_apricitas + theme(legend.position = "top") +
   theme(axis.title.x = element_text(size = 20, hjust = 0.5),
         axis.title.y = element_text(size = 15, vjust = 0.5),
@@ -289,5 +289,171 @@ ANNUAL_SCE_HOUSING_PROB_MOVE_graph <- ggplot() +
   coord_cartesian(clip = "off")
 
 ggsave(dpi = "retina",plot = ANNUAL_SCE_HOUSING_PROB_MOVE_graph, "Homeowner Moving Probabilities.png", type = "cairo-png", width = 9.02, height = 5.76, units = "in") #cairo gets rid of anti aliasing
+
+REDFIN_NATIONAL_DATA <- read.csv("https://raw.githubusercontent.com/Miles-byte/Apricitas/main/Have%20Mortgage%20Rates%20Frozen%20the%20Housing%20Market/REDFIN_NATIONAL_DATA.csv") %>%
+  mutate(date = as.Date(period_begin,"%m/%d/%Y")) %>%
+  filter(is_seasonally_adjusted == "t",property_type_id == "-1") %>%
+  select(date, inventory)
+
+REDFIN_NATIONAL_DATA_INVENTORY_graph <- ggplot() + 
+  annotate("hline", y = 0, yintercept = 0, color = "white", size = .5) +
+  geom_line(data = REDFIN_NATIONAL_DATA, aes(x = date, y = `inventory`/1000000, color = "All Homes For Sale, Redfin Estimate"), size = 1.25) +
+  xlab("Date") +
+  ylab("Housing Inventory, Millions") +
+  scale_y_continuous(labels = scales::number_format(accuracy = 0.5, suffix = "M"), breaks = c(0,0.5,1,1.5,2), limits = c(0,2.25), expand = c(0,0)) +
+  ggtitle("Housing Inventory is Extremely Low") +
+  labs(caption = "Graph created by @JosephPolitano using Redfin data", subtitle = "Rising Interest Rates Have Brought Housing Inventory to Near 10 Year Lows") +
+  theme_apricitas + theme(legend.position = c(.37,.29)) +#, axis.text.x=element_blank(), axis.title.x=element_blank()) +
+  scale_color_manual(name= NULL,values = c("#FFE98F","#00A99D","#6A4C93","#A7ACD9","#3083DC","#9A348E","#00A99D","#EE6055")) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("2012-01-01")-(.1861*(today()-as.Date("2012-01-01"))), xmax = as.Date("2012-01-01")-(0.049*(today()-as.Date("2012-01-01"))), ymin = 0-(.3*2.25), ymax = 0) + #these repeated sections place the logo in the bottom-right of each graph. The first number in all equations is the chart's origin point, and the second number is the exact length of the x or y axis
+  coord_cartesian(clip = "off")
+
+ggsave(dpi = "retina",plot = REDFIN_NATIONAL_DATA_INVENTORY_graph, "Redfin National Inventory Data graph.png", type = "cairo-png", width = 9.02, height = 5.76, units = "in") #cairo gets rid of anti aliasing
+
+FHFA_DATA <- read.csv("https://raw.githubusercontent.com/Miles-byte/Apricitas/main/Have%20Mortgage%20Rates%20Frozen%20the%20Housing%20Market/FHFA_NMDB_MORTGAGE_STATISTICS.csv") %>%
+  filter(MARKET == "All Mortgages" & SERIESID == "AVE_INTRATE" & GEOID == "USA") %>%
+  transmute(date = as.Date(as.yearqtr(PERIOD, "%YQ%q")), value = VALUE1)
+
+FIXED_30YR_MORTGAGE <- fredr(series_id = "MORTGAGE30US", observation_start = as.Date("2013-01-01"))
+
+FIXED_30YR_FHFA_graph <- ggplot() + 
+  annotate("hline", y = 0, yintercept = 0, color = "white", size = .5) +
+  geom_line(data = FHFA_DATA, aes(x = date+45, y = value/100, color = "Average Interest Rate on Outstanding Mortgages"), size = 1.25) +
+  geom_line(data = FIXED_30YR_MORTGAGE, aes(x = date, y = value/100, color = "Average Interest Rate on New 30-Year Mortgage"), size = 1.25) +
+  xlab("Date") +
+  ylab("Interest Rate, %") +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1), breaks = c(0,0.025,0.05,0.075), limits = c(0,.075), expand = c(0,0)) +
+  ggtitle("Low Mortgage Rates as Golden Handcuffs?") +
+  labs(caption = "Graph created by @JosephPolitano using FHFA and Freddie Mac data", subtitle = "Interest Rates on New 30-Year Mortgages Have Surged Above Rates for Average Existing Borrowers") +
+  theme_apricitas + theme(legend.position = c(.37,.29)) +#, axis.text.x=element_blank(), axis.title.x=element_blank()) +
+  scale_color_manual(name= NULL,values = c("#FFE98F","#00A99D","#6A4C93","#A7ACD9","#3083DC","#9A348E","#00A99D","#EE6055")) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("2013-01-01")-(.1861*(today()-as.Date("2013-01-01"))), xmax = as.Date("2013-01-01")-(0.049*(today()-as.Date("2013-01-01"))), ymin = 0-(.3*0.075), ymax = 0) + #these repeated sections place the logo in the bottom-right of each graph. The first number in all equations is the chart's origin point, and the second number is the exact length of the x or y axis
+  coord_cartesian(clip = "off")
+
+ggsave(dpi = "retina",plot = FIXED_30YR_FHFA_graph, "Fixed 30yr FHFA graph.png", type = "cairo-png", width = 9.02, height = 5.76, units = "in") #cairo gets rid of anti aliasing
+
+HOMEOWN_FREE_CLEAR_ZIP <- get_acs(
+  geography = "zcta",
+  variables = "DP04_0092PE",
+  #table = "DP04",
+  cache_table = TRUE,
+  year = 2021,
+  output = "wide",
+  key = Sys.getenv("CENSUS_KEY"),
+  moe_level = 90,
+  survey = "acs5",
+  show_call = TRUE,
+)
+
+ZIP <- zctas(year = 2020)
+options(tigris_use_cache = TRUE)
+
+ZIP_ACS_MERGE <- left_join(ZIP, HOMEOWN_FREE_CLEAR_ZIP, by = c("GEOID20" = "GEOID")) %>%
+  transmute(GEOID20, value = DP04_0092PE, geometry) %>%
+  mutate(value_cut = cut(value, breaks = c(-Inf,30,35,40,45,Inf), labels = c("<30%", "30-35%", "35-40%", "40-45%", ">45%")))
+
+
+# ZipCodeSourceFile = "http://download.geonames.org/export/zip/US.zip"
+# temp <- tempfile()
+# download.file(ZipCodeSourceFile , temp)
+# ZipCodes <- read.table(unz(temp, "US.txt"), sep="\t")
+# unlink(temp)
+# names(ZipCodes) = c("CountryCode", "zip", "PlaceName", 
+#                     "AdminName1", "AdminCode1", "AdminName2", "AdminCode2", 
+#                     "AdminName3", "AdminCode3", "latitude", "longitude", "accuracy")
+# ZipCodes$zip <- str_pad(as.character(ZipCodes$zip), width = 5, pad = "0")
+# 
+# ZipCodes <- ZipCodes %>%
+#   filter(!duplicated(zip))
+
+states <- states(2020, resolution = "500k", cb = TRUE)
+
+PCT_DC <- ggplot() +
+  geom_sf(data = subset(ZIP_ACS_MERGE, GEOID20 %in% get_zctas_by_state(c("DC","MD","VA"))& !is.na(value_cut)), aes(fill = value_cut, color = value_cut)) +
+  geom_sf(data = subset(states, STUSPS %in% c("DC","MD","VA")), color = "black", fill = NA, lwd = 0.65) + # Black borders for states
+  coord_sf(crs = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs") + #albers projection
+  scale_fill_manual(values = c("#EE6055","#F5B041","#FFE98F", "#AED581", "#00A99D"),
+                    guide = "legend", 
+                    labels = c("<30%", "30-35%", "35-40%", "40-45%", ">45%")) +
+  scale_color_manual(values = c("#EE6055","#F5B041","#FFE98F", "#AED581", "#00A99D"),
+                    guide = NULL, 
+                    labels = c("<30%", "30-35%", "35-40%", "40-45%", ">45%")) +
+  ggtitle("    Percent of Homeowners Without a Mortgage") +
+  labs(fill = NULL) +
+  theme_apricitas + theme(legend.position = "right", panel.grid.major=element_blank(), axis.line = element_blank(), axis.text.x = element_blank(),axis.text.y = element_blank(),plot.margin= grid::unit(c(0, 0, 0, 0), "in")) +
+  theme(plot.title = element_text(size = 24,hjust = 0.5))
+  
+ggsave(dpi = "retina",plot = PCT_DC, "PCT DC graph.png", type = "cairo-png", width = 9.02, height = 5.76, units = "in") #cairo gets rid of anti aliasing
+
+PCT_WEST <- ggplot() +
+  geom_sf(data = subset(ZIP_ACS_MERGE, GEOID20 %in% get_zctas_by_state(c("CA","OR","WA","ID","UT","NV","NM","AZ","CO","MT","WY"))& !is.na(value_cut)), aes(fill = value_cut, color = value_cut)) +
+  geom_sf(data = subset(states, STUSPS %in% c("CA","OR","WA","ID","UT","NV","NM","AZ","CO","MT","WY")), color = "black", fill = NA, lwd = 0.65) + # Black borders for states
+  coord_sf(crs = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs") + #albers projection
+  scale_fill_manual(values = c("#EE6055","#F5B041","#FFE98F", "#AED581", "#00A99D"),
+                    guide = "legend", 
+                    labels = c("<30%", "30-35%", "35-40%", "40-45%", ">45%")) +
+  scale_color_manual(values = c("#EE6055","#F5B041","#FFE98F", "#AED581", "#00A99D"),
+                     guide = NULL, 
+                     labels = c("<30%", "30-35%", "35-40%", "40-45%", ">45%")) +
+  ggtitle("    Percent of Homeowners Without a Mortgage") +
+  labs(fill = NULL) +
+  theme_apricitas + theme(legend.position = "right", panel.grid.major=element_blank(), axis.line = element_blank(), axis.text.x = element_blank(),axis.text.y = element_blank(),plot.margin= grid::unit(c(0, 0, 0, 0), "in")) +
+  theme(plot.title = element_text(size = 24,hjust = 0.5))
+
+ggsave(dpi = "retina",plot = PCT_WEST, "PCT WEST.png", type = "cairo-png", width = 9.02, height = 5.76, units = "in")
+
+PCT_NORTHEAST <- ggplot() +
+  geom_sf(data = subset(ZIP_ACS_MERGE, GEOID20 %in% get_zctas_by_state(c("ME","NH","VT","RI","NJ","NY","PA","MA","CT","PA","DC","MD","VA","WV"))& !is.na(value_cut)), aes(fill = value_cut, color = value_cut)) +
+  geom_sf(data = subset(states, STUSPS %in% c("ME","NH","VT","RI","NJ","NY","PA","MA","CT","PA","DC","MD","VA","WV")), color = "black", fill = NA, lwd = 0.65) + # Black borders for states
+  coord_sf(crs = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs") + #albers projection
+  scale_fill_manual(values = c("#EE6055","#F5B041","#FFE98F", "#AED581", "#00A99D"),
+                    guide = "legend", 
+                    labels = c("<30%", "30-35%", "35-40%", "40-45%", ">45%")) +
+  scale_color_manual(values = c("#EE6055","#F5B041","#FFE98F", "#AED581", "#00A99D"),
+                     guide = NULL, 
+                     labels = c("<30%", "30-35%", "35-40%", "40-45%", ">45%")) +
+  ggtitle("    Percent of Homeowners Without a Mortgage") +
+  labs(fill = NULL) +
+  theme_apricitas + theme(legend.position = "right", panel.grid.major=element_blank(), axis.line = element_blank(), axis.text.x = element_blank(),axis.text.y = element_blank(),plot.margin= grid::unit(c(0, 0, 0, 0), "in")) +
+  theme(plot.title = element_text(size = 24,hjust = 0.5))
+
+ggsave(dpi = "retina",plot = PCT_NORTHEAST, "PCT NORTHEAST.png", type = "cairo-png", width = 9.02, height = 5.76, units = "in")
+
+PCT_SOUTH <- ggplot() +
+  geom_sf(data = subset(ZIP_ACS_MERGE, GEOID20 %in% get_zctas_by_state(c("NC","SC","GA","AL","MS","FL","LA","TX","TN","AR","OK"))& !is.na(value_cut)), aes(fill = value_cut, color = value_cut)) +
+  geom_sf(data = subset(states, STUSPS %in% c("NC","SC","GA","AL","MS","FL","LA","TX","TN","AR","OK")), color = "black", fill = NA, lwd = 0.65) + # Black borders for states
+  coord_sf(crs = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs") + #albers projection
+  scale_fill_manual(values = c("#EE6055","#F5B041","#FFE98F", "#AED581", "#00A99D"),
+                    guide = "legend", 
+                    labels = c("<30%", "30-35%", "35-40%", "40-45%", ">45%")) +
+  scale_color_manual(values = c("#EE6055","#F5B041","#FFE98F", "#AED581", "#00A99D"),
+                     guide = NULL, 
+                     labels = c("<30%", "30-35%", "35-40%", "40-45%", ">45%")) +
+  ggtitle("    Percent of Homeowners Without a Mortgage") +
+  labs(fill = NULL) +
+  theme_apricitas + theme(legend.position = "right", panel.grid.major=element_blank(), axis.line = element_blank(), axis.text.x = element_blank(),axis.text.y = element_blank(),plot.margin= grid::unit(c(0, 0, 0, 0), "in")) +
+  theme(plot.title = element_text(size = 24,hjust = 0.5))
+
+ggsave(dpi = "retina",plot = PCT_SOUTH, "PCT SOUTH.png", type = "cairo-png", width = 9.02, height = 5.76, units = "in")
+
+PCT_MIDWEST <- ggplot() +
+  geom_sf(data = subset(ZIP_ACS_MERGE, GEOID20 %in% get_zctas_by_state(c("OH","MI","WI","MN","IA","MO","KS","NE","SD","ND","IL","KY","IN"))& !is.na(value_cut)), aes(fill = value_cut, color = value_cut)) +
+  geom_sf(data = subset(states, STUSPS %in% c("OH","MI","WI","MN","IA","MO","KS","NE","SD","ND","IL","KY","IN")), color = "black", fill = NA, lwd = 0.65) + # Black borders for states
+  coord_sf(crs = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs") + #albers projection
+  scale_fill_manual(values = c("#EE6055","#F5B041","#FFE98F", "#AED581", "#00A99D"),
+                    guide = "legend",
+                    breaks = c("<30%", "30-35%", "35-40%", "40-45%", ">45%"),
+                    labels = c("<30%", "30-35%", "35-40%", "40-45%", ">45%")) +
+  scale_color_manual(values = c("#EE6055","#F5B041","#FFE98F", "#AED581", "#00A99D"),
+                     guide = NULL,
+                     breaks = c("<30%", "30-35%", "35-40%", "40-45%", ">45%"),
+                     labels = c("<30%", "30-35%", "35-40%", "40-45%", ">45%")) +
+  ggtitle("    Percent of Homeowners Without a Mortgage") +
+  labs(fill = NULL) +
+  theme_apricitas + theme(legend.position = "right", panel.grid.major=element_blank(), axis.line = element_blank(), axis.text.x = element_blank(),axis.text.y = element_blank(),plot.margin= grid::unit(c(0, 0, 0, 0), "in")) +
+  theme(plot.title = element_text(size = 24,hjust = 0.5))
+
+ggsave(dpi = "retina",plot = PCT_MIDWEST, "PCT MIDWEST.png", type = "cairo-png", width = 9.02, height = 5.76, units = "in")
+
 
 #ANNUAL SURVEY MOVING EXPECTATIONS
