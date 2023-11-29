74119ef27e7d64cf9382e5762638c1d6d6e9595d
Miles-byte <58167625+Miles-byte@users.noreply.github.com>
2023-11-29 04:21:34 -0500

    Updating Energy Export Data

--- America Net Energy Export/America's Energy Exports.R
+++ America Net Energy Export/America's Energy Exports.R
@@ -9,6 +9,9 @@ apricitas_logo_rast <- rasterGrob(apricitas_logo, interpolate=TRUE)
 install_github("keberwein/blscrapeR")
 library(blscrapeR)
 
+devtools::install_github("jameelalsalam/eia2")
+library("eia2")
+
 US_ENERGY_EXPORTS <- getCensus(
   name = "timeseries/intltrade/exports/hs",
   vars = c("MONTH", "YEAR", "ALL_VAL_MO", "E_COMMODITY", "CTY_CODE"), 
@@ -47,7 +50,7 @@ US_ENERGY_NET_EXP_Graph <- ggplot() + #plotting nat gas exports
   ylab("Dollars") +
   ggtitle("The US is Now an Energy Exporter") +
   labs(caption = "Graph created by @JosephPolitano using Census data",subtitle = "America's Energy Exports Have Risen Significantly Over the Last Decade") +
-  theme_apricitas + theme(legend.position = c(.40,.90)) +
+  theme_apricitas + theme(legend.position = c(.40,.85)) +
   scale_color_manual(name= "Dashed = Monthly Annualized, Solid = 12M Moving Total",values = c("#FFE98F","#00A99D","#A7ACD9","#9A348E","#EE6055","#3083DC","RED")) +
   annotation_custom(apricitas_logo_rast, xmin = as.Date("2013-01-01")-(.1861*(today()-as.Date("2013-01-01"))), xmax = as.Date("2013-01-01")-(0.049*(today()-as.Date("2013-01-01"))), ymin = -300-(.3*450), ymax = -300) +
   coord_cartesian(clip = "off")
@@ -128,14 +131,14 @@ US_PRIMARY_ENERGY_NET_EXPORTS_Graph <- ggplot() + #plotting nat gas exports
   annotate("hline", y = 0, yintercept = 0, color = "white", size = 0.5) +
   geom_line(data = US_PETROL_EXPORTS, aes(x = date, y = value, color = "Crude Oil and Petroleum Products"), size = 1.25) +
   geom_line(data = US_NATGAS_EXPORTS, aes(x = date, y = value, color = "Natural Gas"), size = 1.25) +
-  geom_line(data = US_TOTAL_EXPORTS, aes(x = date, y = value, color = "Total Primary Energy"), size = 1.25) +
+  geom_line(data = US_TOTAL_EXPORTS, aes(x = date, y = value, color = "Total Primary Energy"), size = 2.25) +
   xlab("Date") +
   scale_y_continuous(labels = scales::number_format(suffix = "QBTU", accuracy = 1),limits = c(-32,ceiling(max(US_TOTAL_EXPORTS$value))), breaks = c(-30,-20,-10,0,10), expand = c(0,0)) +
   ylab("Quadrillions of BTUs") +
-  ggtitle("America's Rise as an Energy Exporter") +
+  ggtitle("America's Energy Export Boom") +
   labs(caption = "Graph created by @JosephPolitano using EIA data",subtitle = "America's Has Become a Large Net Energy Exporter Since the Start of the Pandemic") +
   theme_apricitas + theme(legend.position = c(.25,.20)) +
-  scale_color_manual(name= "Primary Energy Net Exports\n(Quadrillion BTUs, Annual)",values = c("#FFE98F","#00A99D","#EE6055","#A7ACD9","#9A348E","#3083DC","RED"), breaks = c("Total Primary Energy","Crude Oil and Petroleum Products","Natural Gas")) +
+  scale_color_manual(name= "Primary Energy Net Exports\n(Quadrillion BTUs, Annual)",values = c("#FFE98F","#00A99D","#EE6055","#A7ACD9","#9A348E","#3083DC","RED"), breaks = c("Total Primary Energy","Crude Oil and Petroleum Products","Natural Gas"), guide = guide_legend(override.aes = list(lwd = c(2.25,1.25,1.25)))) +
   annotation_custom(apricitas_logo_rast, xmin = as.Date("1949-01-01")-(.1861*(today()-as.Date("1949-01-01"))), xmax = as.Date("1949-01-01")-(0.049*(today()-as.Date("1949-01-01"))), ymin = -32-(.3*38), ymax = -32) +
   coord_cartesian(clip = "off")
 
@@ -164,7 +167,7 @@ US_NAT_GAS_PRODUCTION_CONSUMPTION_Graph <- ggplot() + #plotting nat gas exports
   ylab("Billions of Cubic Feet per Day") +
   ggtitle("America's Natural Gas Export Boom") +
   labs(caption = "Graph created by @JosephPolitano using EIA data",subtitle = "US Natural Gas Production is Growing Faster than Domestic Consumption") +
-  theme_apricitas + theme(legend.position = c(.40,.90)) +
+  theme_apricitas + theme(legend.position = c(.40,.85)) +
   scale_color_manual(name= "12M Moving Average",values = c("#FFE98F","#00A99D","#A7ACD9","#9A348E","#EE6055","#3083DC","RED")) +
   annotation_custom(apricitas_logo_rast, xmin = as.Date("2000-01-01")-(.1861*(today()-as.Date("2000-01-01"))), xmax = as.Date("2000-01-01")-(0.049*(today()-as.Date("2000-01-01"))), ymin = 0-(.3*125), ymax = 0) +
   coord_cartesian(clip = "off")
@@ -211,7 +214,7 @@ US_NAT_GAS_LNG_PIPELINE_EXPORTS_Graph <- ggplot() + #plotting nat gas exports
   ylab("Billions of Cubic Feet per Day") +
   ggtitle("America's Natural Gas Export Boom") +
   labs(caption = "Graph created by @JosephPolitano using EIA data",subtitle = "America Has Become a Major Net Exporter of Natural Gas Especially via LNG") +
-  theme_apricitas + theme(legend.position = c(.40,.90)) +
+  theme_apricitas + theme(legend.position = c(.40,.85)) +
   scale_color_manual(name= "Dashed = Monthly, Solid = 12M Moving Average",values = c("#FFE98F","#00A99D","#A7ACD9","#9A348E","#EE6055","#3083DC","RED")) +
   annotation_custom(apricitas_logo_rast, xmin = as.Date("2000-01-01")-(.1861*(today()-as.Date("2000-01-01"))), xmax = as.Date("2000-01-01")-(0.049*(today()-as.Date("2000-01-01"))), ymin = -12.5-(.3*27.5), ymax = -12.5) +
   coord_cartesian(clip = "off")
@@ -244,17 +247,35 @@ FINISHED_MOTOR_GASOLINE_SUPPLIED <- eia1_series("PET.MGFUPUS2.M") %>%
   mutate(rollmean = c(NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,rollmean(value,12)))
 
 FINISHED_MOTOR_GASOLINE_SUPPLIED_Graph <- ggplot() + #plotting US Crude Production
+  #geom_line(data=filter(FINISHED_MOTOR_GASOLINE_SUPPLIED, date >= as.Date("1945-01-01")), aes(x=date,y= value/1000, color= "US Finished Motor Gasoline Supplied"), size = 0.75, linetype = "dashed", alpha = 0.5) +
+  geom_line(data=filter(FINISHED_MOTOR_GASOLINE_SUPPLIED, date >= as.Date("1945-01-01")), aes(x=date,y= rollmean/1000, color= "US Finished Motor Gasoline Supplied 12MMA"), size = 1.25) +
+  xlab("Date") +
+  scale_y_continuous(labels = scales::number_format(suffix = " MMbbl/d", accuracy = 1), limits = c(0,10),breaks = c(1,2,3,4,5,6,7,8,9,10), expand = c(0,0)) +
+  ylab("Mbbl Per Day") +
+  ggtitle("US Gasoline Consumption") +
+  labs(caption = "Graph created by @JosephPolitano using EIA data",subtitle = "US Gasoline Consumption May Have Peaked With the Pandemic") +
+  theme_apricitas + theme(legend.position = c(.75,.2)) +
+  scale_color_manual(name= NULL ,values = c("#FFE98F","#00A99D","#EE6055","#A7ACD9","#9A348E")) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("1945-01-01")-(.1861*(today()-as.Date("1945-01-01"))), xmax = as.Date("1945-01-01")-(0.049*(today()-as.Date("1945-01-01"))), ymin = 0-(.3*10), ymax = 0) + #these repeated sections place the logo in the bottom-right of each graph. The first number in all equations is the chart's origin point, and the second number is the exact length of the x or y axis
+  coord_cartesian(clip = "off")
+
+ggsave(dpi = "retina",plot = FINISHED_MOTOR_GASOLINE_SUPPLIED_Graph, "Finished Motor Gasoline Supplied Graph.png", type = "cairo-png", width = 9.02, height = 5.76, units = "in") #cairo gets rid of anti aliasing
+
+FINISHED_MOTOR_GASOLINE_SUPPLIED_2018_Graph <- ggplot() + #plotting US Crude Production
   geom_line(data=filter(FINISHED_MOTOR_GASOLINE_SUPPLIED, date >= as.Date("2018-01-01")), aes(x=date,y= value/1000, color= "US Finished Motor Gasoline Supplied"), size = 0.75, linetype = "dashed", alpha = 0.5) +
   geom_line(data=filter(FINISHED_MOTOR_GASOLINE_SUPPLIED, date >= as.Date("2018-01-01")), aes(x=date,y= rollmean/1000, color= "US Finished Motor Gasoline Supplied"), size = 1.25) +
   xlab("Date") +
-  scale_y_continuous(labels = scales::number_format(suffix = " MMbbl", accuracy = 1), limits = c(5.75,10),breaks = c(6,7,8,9,10), expand = c(0,0)) +
+  scale_y_continuous(labels = scales::number_format(suffix = " MMbbl/d", accuracy = 1), limits = c(5.75,10),breaks = c(1,2,3,4,5,6,7,8,9,10), expand = c(0,0)) +
   ylab("Mbbl Per Day") +
-  ggtitle("America's Oil Recovery") +
-  labs(caption = "Graph created by @JosephPolitano using EIA data",subtitle = "US Oil Production Is Closing in on Record High Levels") +
+  ggtitle("US Gasoline Consumption") +
+  labs(caption = "Graph created by @JosephPolitano using EIA data",subtitle = "US Gasoline Consumption May Have Peaked With the Pandemic") +
   theme_apricitas + theme(legend.position = c(.75,.2)) +
   scale_color_manual(name= "Dashed = Monthly, Solid = 12M Moving Average" ,values = c("#FFE98F","#00A99D","#EE6055","#A7ACD9","#9A348E")) +
   annotation_custom(apricitas_logo_rast, xmin = as.Date("2018-01-01")-(.1861*(today()-as.Date("2018-01-01"))), xmax = as.Date("2018-01-01")-(0.049*(today()-as.Date("2018-01-01"))), ymin = 5.75-(.3*4.25), ymax = 5.75) + #these repeated sections place the logo in the bottom-right of each graph. The first number in all equations is the chart's origin point, and the second number is the exact length of the x or y axis
   coord_cartesian(clip = "off")
+
+ggsave(dpi = "retina",plot = FINISHED_MOTOR_GASOLINE_SUPPLIED_2018_Graph, "Finished Motor Gasoline Supplied 2018 Graph.png", type = "cairo-png", width = 9.02, height = 5.76, units = "in") #cairo gets rid of anti aliasing
+
 
 ALL_EMPLOYEES_OIL_GAS_2000 <- bls_api("CES1021100001", startyear = 2000, registrationKey = Sys.getenv("BLS_KEY")) %>% 
   mutate(date = as.Date(as.yearmon(paste(periodName, year), "%b %Y")))
@@ -296,6 +317,62 @@ ALL_EMPLOYEES_OIL_GAS_Graph <- ggplot(ALL_EMPLOYEES_OIL_GAS_RBIND, aes(fill=name
   coord_cartesian(clip = "off")
 
 ggsave(dpi = "retina",plot = ALL_EMPLOYEES_OIL_GAS_Graph, "All Employees Oil Gas Graph.png", type = "cairo-png", width = 9.02, height = 5.76, units = "in") #cairo gets rid of anti aliasing
+
+LNG_EXPORT_DATES <- data.frame(date = seq(as.Date("2016-01-01"), as.Date("2029-12-01"), by="month"))
+LNG_EXPORT_DATA <- read.csv("https://raw.githubusercontent.com/Miles-byte/Apricitas/main/America%20Net%20Energy%20Export/LNG_EXPORT_CAPACITY.csv") %>%
+  mutate(PEAK.CAPACITY = as.numeric(PEAK.CAPACITY)) %>%
+  mutate(date = as.Date(date)) %>%
+  pivot_wider(names_from = category, values_from = PEAK.CAPACITY) %>%
+  full_join(LNG_EXPORT_DATES,., by = "date") %>%
+  mutate(across(where(is.numeric), ~replace_na(., 0))) %>%
+  mutate(across(where(is.numeric), cumsum)) %>%
+  group_by(date) %>%
+  filter(row_number() == max(row_number())) %>%
+  ungroup() %>%
+  select(-name) %>%
+  pivot_longer(-date) %>%
+  mutate(name = gsub("Proposed, Partially/Fully Approved, Not Yet Under Construction","Permitting in Progress, No Start Date",name)) %>%
+  mutate(name = gsub("Under Construction","Currently Under Construction",name)) %>%
+  mutate(name = factor(name, levels = rev(c("LNG Export Capacity","Currently Under Construction","Permitting in Progress, No Start Date"))))
+ 
+#UPDATE WITH LIQUEFACTION CAPACITY FILE
+
+LNG_EXPORT_CAPACITY_Graph <- ggplot() +
+  geom_bar(data = filter(LNG_EXPORT_DATA, name != "Permitting in Progress, No Start Date" & date < as.Date("2029-01-01")), aes(fill = name, x= date, y = value), position = "stack", stat = "identity", size = 0, color = NA, width = 32) +
+  geom_rect(data = filter(LNG_EXPORT_DATA, name == "Permitting in Progress, No Start Date"), 
+            aes(xmin = as.Date("2030-01-01"), xmax = as.Date("2031-01-01") , ymin = 22.62, ymax = 22.62 + value, fill = name), size = 0, color = NA, width = 32) +
+  xlab("Date") +
+  geom_vline(xintercept = as.Date("2023-10-01"), linetype = "dashed", color = "white", size = 1.25) +
+  annotate("text", x = as.Date("2022-12-01"), y = Inf, label = "History", vjust = 2, color = "white", size = 5) +
+  annotate("text", x = as.Date("2024-10-01"), y = Inf, label = "Forecast", vjust = 2, color = "white", size = 5) + 
+  scale_y_continuous(labels = scales::number_format(suffix = "Bcf/d", accuracy = 1),limits = c(0,45), breaks = c(0,10,20,30,40), expand = c(0,0)) +
+  ylab("Billions of Cubic Feet per Day") +
+  ggtitle("America's Growing LNG Export Capacity") +
+  labs(caption = "Graph created by @JosephPolitano using EIA data",subtitle = "US Liquefied Natural Gas Export Capacity is Growing—With Many Facilities in the Pipeline") +
+  theme_apricitas + theme(legend.position = c(.25,.89)) +
+  scale_fill_manual(name = NULL, values = c("#FFE98F","#00A99D","#EE6055"), breaks = c("LNG Export Capacity","Currently Under Construction","Permitting in Progress, No Start Date")) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("2016-01-01")-(.1861*(as.Date("2031-01-01")-as.Date("2016-01-01"))), xmax = as.Date("2016-01-01")-(0.049*(as.Date("2031-01-01")-as.Date("2016-01-01"))), ymin = 0-(.3*45), ymax = 0) +
+  coord_cartesian(clip = "off")
+
+ggsave(dpi = "retina",plot = LNG_EXPORT_CAPACITY_Graph, "LNG Export Capacity Graph.png", type = "cairo-png", width = 9.02, height = 5.76, units = "in") #cairo gets rid of anti aliasing
+
+CRUDE_OIL_FOOTAGE_DRILLED <- eia1_series("TOTAL.OGPFPUS.M") %>%
+  transmute(date = as.Date(paste0(period,"-01")), value = as.numeric(value)) %>%
+  arrange(date)
+
+CRUDE_OIL_FOOTAGE_DRILLED_Graph <- ggplot() +
+  geom_line(data=filter(CRUDE_OIL_FOOTAGE_DRILLED, date >= as.Date("2005-01-01")), aes(x=date,y= value/1000, color= "US Crude Oil Wells, Total Footage Drilled"), size = 1.25) +
+  xlab("Date") +
+  scale_y_continuous(labels = scales::number_format(suffix = "MMft", accuracy = 1),limits = c(0,25), breaks = c(5,10,15,20,25), expand = c(0,0)) +
+  ylab("Millions of Feet") +
+  ggtitle("America's Oil Drilling Slowdown") +
+  labs(caption = "Graph created by @JosephPolitano using EIA data",subtitle = "US Drilling Levels are Near Modern Highs, but Are Now Declining") +
+  theme_apricitas + theme(legend.position = c(.7,.1)) +
+  scale_color_manual(name = NULL, values = c("#FFE98F","#00A99D","#EE6055")) +
+  annotation_custom(apricitas_logo_rast, xmin = as.Date("2005-01-01")-(.1861*(today()-as.Date("2005-01-01"))), xmax = as.Date("2005-01-01")-(0.049*(today()-as.Date("2005-01-01"))), ymin = 0-(.3*25), ymax = 0) +
+  coord_cartesian(clip = "off")
+
+ggsave(dpi = "retina",plot = CRUDE_OIL_FOOTAGE_DRILLED_Graph, "Crude Oil Footage Drilled Graph.png", type = "cairo-png", width = 9.02, height = 5.76, units = "in") #cairo gets rid of anti aliasing
 
 
 p_unload(all)  # Remove all add-ons
